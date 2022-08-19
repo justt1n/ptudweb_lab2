d24eae426e5cfb6d6a91d4dcd7bb10d0f1aaf673
justt1n <tinb1910464@student.ctu.edu.vn>
2022-08-19 23:14:46 +0700

    config Error Handler

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 const app = express();
 
 app.use(cors());
@@ -11,7 +12,21 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", contactsRouter);
 
+
+app.use((req, res, next) =>  {
+    //this code will run when no route def
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    //Centralized error handling Middleware
+    //When route call next(error), the Middleware will handle this
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
 
