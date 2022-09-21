93af7df0d6508429f5e314510eb5daf62b978b13
justt1n <tinb1910464@student.ctu.edu.vn>
2022-09-21 14:47:36 +0700

    fix error handler

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) =>  {
     return next(new ApiError(404, "Resource not found"));
 });
 
-app.use((err, req, res, next) => {
+app.use((error, req, res, next) => {
     //Centralized error handling Middleware
     //When route call next(error), the Middleware will handle this
     return res.status(error.statusCode || 500).json({
